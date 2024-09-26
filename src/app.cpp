9dd864a440c662312b051a12d89b538aa0e60dd6
Willis Chiejina <ashieduwillis@gmail.com>
2024-09-26 18:02:43 +0100

    Edits to app.cpp and data.h

--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "Renderer.h"
+#include "data.h"
 
 int main (int argc, char* argv[])
 {
     std::cout << "gWindow" << std::endl;
+    data area_0(0, 1, 1);
     return 0;
 }
--- src/data.h
+++ src/data.h
@@ -8,14 +8,27 @@ struct point
 class data
 {
 public:
-    data(unsigned int ID, int v0, int v1) : m_ID(ID), m_point(v0, v1) {}
+    data(unsigned int ID, int v0, int v1) : m_ID(ID), m_point(v0, v1) {++s_dataStructSize;}
     ~data()
     {
+        --s_dataStructSize;
         m_next = nullptr;
         m_prev = nullptr;
+    }
+    void addDataNext(data *dn)
+    {
+        (data*)m_next;
+        m_next = dn;
+    }
+    void addDataPrev(data *dn)
+    {
+        (data*)m_prev;
+        m_prev = dn;
     }
 private:
     unsigned int m_ID;
     point m_point;
     data *m_next, *m_prev;
+    static long int s_dataStructSize;
 };
+long int data::s_dataStructSize = 0;
